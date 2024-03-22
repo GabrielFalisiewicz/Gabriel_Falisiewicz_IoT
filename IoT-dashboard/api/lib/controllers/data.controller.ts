6b529af91e3c0a3fa948340b8bcd458d5699da02
Gabriel <gabrielfalisiewicz@gmail.com>
2024-03-22 14:42:02 +0100

    com

---
+++ IoT-dashboard/api/lib/controllers/data.controller.ts
@@ -0,0 +1,71 @@
+import Controller from '../interfaces/controller.interface';
+import { Request, Response, NextFunction, Router } from 'express';
+
+let testArr = [4,5,6,3,5,3,7,5,13,5,6,4,3,6,3,6];
+
+class DataController implements Controller {
+    public path = '/api/data';
+    public router = Router();
+ 
+    constructor() {
+        this.initializeRoutes();
+    }
+ 
+    private initializeRoutes() {
+        this.router.get(`${this.path}/latest`, this.getLatestReadingsFromAllDevices);
+        this.router.post(`${this.path}/:id`, this.addData);
+        this.router.get(`${this.path}/:id`, this.getDataById);
+        this.router.get(`${this.path}/:id/latest`, this.getLatestDataById);
+        this.router.get(`${this.path}/:id/:num`, this.getRangeDataById);
+        this.router.delete(`${this.path}/all`, this.deleteAllData);
+        this.router.delete(`${this.path}/:id`, this.deleteDataById);
+
+    }
+
+    private getLatestReadingsFromAllDevices = async (request: Request, response: Response, next: NextFunction) => {
+        const id = Number(request.params.id);
+        const data = testArr;
+        console.log("Text")
+        response.status(200).json(data);
+    };
+
+    private addData = async(request: Request, response: Response, next: NextFunction) => {
+        const { elem } = request.body;
+        let data = testArr;
+        data.push(elem);
+        response.status(200).json(data); 
+    };
+
+    private getDataById = async (request: Request, response: Response, next: NextFunction) => {
+        const id = Number(request.params.id);
+        const data = testArr[id];
+        response.status(200).json(data);
+    };
+
+    private getLatestDataById = async (request: Request, response: Response, next: NextFunction) => {
+        const id = Number(request.params.id);
+        const data = Math.max(...testArr);
+        response.status(200).json(data);
+    };
+
+    private getRangeDataById = async (request: Request, response: Response, next: NextFunction) => {
+        const id = Number(request.params.id);
+        const num = Number(request.params.num);
+        const data = testArr.slice(id, id + num);
+        response.status(200).json(data);
+    };
+
+    private deleteAllData = async (request: Request, response: Response, next: NextFunction) => {
+        testArr = [];
+        response.status(200).json({message: 'All data deleted.'});
+    };
+
+    private deleteDataById = async (request: Request, response: Response, next: NextFunction) => {
+        const id = Number(request.params.id);
+        testArr.splice(id, 1);
+        response.status(200).json({message: `Data at index ${id} deleted.`});
+    };
+
+ }
+ 
+ export default DataController;
--- IoT-dashboard/api/lib/index.ts
+++ IoT-dashboard/api/lib/index.ts
@@ -1,5 +1,11 @@
 import App from './app';
+import IndexController from "./controllers/index.controller";
+import DataController from './controllers/data.controller';
 
-const app: App = new App();
+const app: App = new App([
+   new DataController(),
+   new IndexController()
+  
+]);
 
 app.listen();
